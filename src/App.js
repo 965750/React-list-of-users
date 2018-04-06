bf4ecd21c0dfc82bded2a7f6e3a7bd11e034f0ff
965750 <aventhelast@gmail.com>
2018-04-06 10:32:40 +0200

    css added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Users />
+        <div className="allCont">
+          <div className="inpCont">
+            <div className="btnAdd">
+              <i class="fas fa-plus-circle iAdd"></i>
+              <span>Add user</span>
+            </div>
+            <form>
+              <input type="text" placeholder="Name..." />
+              <input type="text" placeholder="Email..." />
+              <input type="submit" value="Add" />  
+            </form>
+            <div className="errorCont">
+              <span>Just an simple error</span>
+            </div>
+          </div>
+          <div className="titleCont">
+            <div className="listCol">
+              <h4>LP</h4>
+            </div>
+            <div className="listCol">
+              <h4>User</h4>
+            </div>
+            <div className="listCol">
+              <h4>E-Mail</h4>
+            </div>
+            <div className="listCol">
+            </div>
+          </div>
+          <Users />
       </div>
+    </div>
     );
   }
 }
