96b2c70388f5bc369947ef1444f00d497878342a
Adrian Zajac <aventhelast@gmail.com>
2018-04-06 02:34:57 +0200

    json added

--- src/users/Users.js
+++ src/users/Users.js
@@ -8,15 +8,36 @@ class Users extends Component {
             {name:"Daria", age:22},
             {name:"Magda", age:37}
         ],
+        contacts: [],
         title:"Users List"
+    }
+    componentDidMount(){
+        this.fetchData();
+    }
+    fetchData(){
+        fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+        .then(resp => resp.json())
+        .then(data => data.results.map(user => (
+            {
+                name: `${user.name.first} ${user.name.last}`,
+                username: `${user.login.username}`,
+                email: `${user.email}`,
+                location: `${user.location.street}, ${user.location.city}`
+            }
+        )))
+        .then(contacts => this.setState({
+            contacts,
+            isLoading: false
+        }))
+        .catch(error => console.log('parsing failed', error))
     }
     render (){
         return (
             <div>
                 <h1>{this.state.title}</h1>
                 {
-                    this.state.users.map((user)=>{
-                        return <User age={user.age}>{user.name}</User>    
+                    this.state.contacts.map((user)=>{
+                        return <User key={user.username} age={user.username}>{user.name}</User>    
                     })
                 }
             </div>
